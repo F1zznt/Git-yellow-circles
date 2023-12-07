cb340cc787515aa3371ab20e341297fd91d9d477
F1zznt <oleg88724@gmail.com>
2023-12-07 23:27:06 +0300

    first commit

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 import sys
 import random
-from UI import Ui_MainWindow
 
 
-class Example(QMainWindow, Ui_MainWindow):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("Git и случайные окружности")
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle("Git и жёлтые окружности")
         self.SCREEN_SIZE = [680, 480]
         self.flag = False
         self.coords = []
         self.pushButton.clicked.connect(self.draw)
 
     def draw(self):
-        self.figure = "circle"
+        self.figure = 'circle'
         self.size = random.randint(10, 100)
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.color = 'yellow'
         self.flag = True
         self.update()
 
     def paintEvent(self, event):
         if self.flag:
-            paint = QPainter()
-            paint.begin(self)
-            paint.setPen(QColor(*self.color))
-            paint.setBrush(QColor(*self.color))
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor(self.color))
+            qp.setBrush(QColor(self.color))
             self.x = random.randint(100, self.SCREEN_SIZE[0] - 100)
             self.y = random.randint(100, self.SCREEN_SIZE[1] - 100)
             if self.figure == 'circle':
-                paint.drawEllipse(self.x, self.y, self.size, self.size)
-            paint.end()
+                qp.drawEllipse(self.x, self.y, self.size, self.size)
+            qp.end()
 
 
 if __name__ == '__main__':
